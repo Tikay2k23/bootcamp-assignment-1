ab611e97e24c747a3b80b5a41fa1b5523673f8a9
Tikay2k23 <markangeloyakit@gmail.com>
2025-02-15 19:24:18 +0800

    Update app.py
    wow

--- app.py
+++ app.py
@@ -8,3 +8,4 @@ print(f"{num1} * {num2} = {num1 * num2}")
 print(f"{num1} / {num2} = {num1 / num2}" if num2 != 0 else "Division by zero is not allowed")
 print(f"{num1} % {num2} = {num1 % num2}" if num2 != 0 else "Modulo by zero is not allowed")
 print(f"{num1} ** {num2} = {num1 ** num2}")
+"wow"
