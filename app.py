b597afe4b4b7abeb3445c09a471f1885ea629749
Tikay2k23 <markangeloyakit@gmail.com>
2025-02-15 18:28:34 +0800

    updated changes

---
+++ app.py
@@ -0,0 +1,10 @@
+num1 = float(input("Enter the first number: "))
+num2 = float(input("Enter the second number: "))
+
+
+print(f"{num1} + {num2} = {num1 + num2}")
+print(f"{num1} - {num2} = {num1 - num2}")
+print(f"{num1} * {num2} = {num1 * num2}")
+print(f"{num1} / {num2} = {num1 / num2}" if num2 != 0 else "Division by zero is not allowed")
+print(f"{num1} % {num2} = {num1 % num2}" if num2 != 0 else "Modulo by zero is not allowed")
+print(f"{num1} ** {num2} = {num1 ** num2}")
